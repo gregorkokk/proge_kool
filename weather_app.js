05b0f28e66622630be6c428d10396fe6f8e67d49
gregorkokk <124361279+gregorkokk@users.noreply.github.com>
2023-03-30 15:51:42 +0300

    Create weather_app.js

---
+++ weather_app.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const weatherInfo = document.getElementById('weather-info');
+
+const apiKey = 'your_openweathermap_api_key';
+
+searchBtn.addEventListener('click', () => {
+  const city = searchInput.value;
+  if (city) {
+    fetchWeather(city);
+  }
+});
+
+async function fetchWeather(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+  const data = await response.json();
+
+  if (data.cod === 200) {
+    displayWeather(data);
+  } else {
+    alert('City not found');
+  }
+}
+
+function displayWeather(data) {
+  const { name, main, weather } = data;
+  const temp = main.temp.toFixed(1);
+  const description = weather[0].description;
+
+  weatherInfo.innerHTML = `
+    <h2>${name}</h2>
+    <p>${temp}°C</p>
+    <p>${description}</p>
+  `;
+}
